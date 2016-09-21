bd173edbb7e59e6db97efc537e728d61080f900b
Arash Hatami <hatamiarash7@gmail.com>
2016-09-21 13:13:19 +0330

    Add Files

---
+++ usb-sender.py
@@ -0,0 +1,24 @@
+import serial
+import time
+
+t_end = time.time() + 120
+ser = serial.Serial(
+	port = '/dev/ttyUSB0', #for usb
+        #port = '/dev/ttyAMA0', #for gpio
+	baudrate=9600,
+	bytesize=serial.EIGHTBITS,
+	parity=serial.PARITY_NONE,
+	stopbits=serial.STOPBITS_ONE,
+	xonxoff=serial.XOFF,
+	rtscts=False,
+	dsrdtr=False
+	)
+ser.close()
+ser.open()
+print("write command")
+while time.time() < t_end:
+	Message = raw_input("Enter Message : ")
+	ser.write(str(Message))
+	ser.write('\n')
+	time.sleep(0.1)
+print("done")
